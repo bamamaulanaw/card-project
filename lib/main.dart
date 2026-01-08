c91d978cfa834c362e11dfd1d16aaa8e4d6ee855
bamamaulanaw <bamamaulanaw@gmail.com>
2026-01-08 12:53:38 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:card_project/awan_card.dart';
+import 'package:card_project/food_card.dart';
+import 'package:card_project/lorem_card.dart';
+import 'package:card_project/menu_card.dart';
+import 'package:card_project/title_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +15,29 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CardDesign(),
+                  SizedBox(width: 5),
+                  FoodCard(),
+                  SizedBox(width: 5),
+                  LoremCard(),
+                  SizedBox(width: 5),
+                  AwanCard(),
+                  SizedBox(width: 5),
+                ],
+              ),
+              TitleCard(),
+            ],
+          ),
         ),
       ),
     );
